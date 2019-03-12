74ca6666f755a3207a5258188a9b00426d445061
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-12 13:08:35 -0400

    added Author model and added book admin model

--- core/models.py
+++ core/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+
 class Book(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library.)"""
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
-    author = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author)
     description = models.TextField(null=True, blank=True)
     access_online = models.URLField(max_length=200)
     date_added = models.DateField(null=True, blank=True)
@@ -18,3 +28,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.slug)])
+
+
+    
