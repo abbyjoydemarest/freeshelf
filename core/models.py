602643afb689d81d4425404cdc309211c9c2bdb0
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-11 23:38:16 -0400

    added class meta and __str__ to model Books

--- core/models.py
+++ core/models.py
@@ -9,3 +9,9 @@ class Books(models.Model):
     description = models.TextField(null=True, blank=True)
     access_online = models.CharField(max_length=200)
     date_added = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.title
