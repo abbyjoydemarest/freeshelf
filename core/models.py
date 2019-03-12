217a48d26bf1e4f5ff0b2b7f55a1ef996f9b42b2
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-11 23:34:54 -0400

    added models to models.py for books

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Books(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library.)"""
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    access_online = models.CharField(max_length=200)
+    date_added = models.DateField(null=True, blank=True)
--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#to redirect the base url to your application
+from django.views.generic import RedirectView
+#static for url mapping to static files
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
+    path('', RedirectView.as_view(url='/core/', permanent=True))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
