e2ee69cd58d740819c190583fc932715e87a4a3c
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-13 12:38:08 -0400

    all books is correctly showing the url for the author and the url for the category

--- core/models.py
+++ core/models.py
@@ -3,41 +3,39 @@ from django.urls import reverse
 
 # Create your models here.
 
+class Author(models.Model):
+    
+    author = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.author)
+
+
+class Category(models.Model):
+
+    book_category = models.CharField(max_length=200, null=True, blank=True)
+    
+    
+    def __str__(self):
+        return str(self.book_category)
+
 class Book(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library.)"""
     title = models.CharField(max_length=200, null=True, blank=True)
     slug = models.SlugField(max_length=200, blank=True)
-    author = models.CharField(max_length=200, null=True, blank=True)
+    author = models.ManyToManyField(Author)
     description = models.TextField(null=True, blank=True)
     access_online = models.URLField(max_length=200, null=True, blank=True)
-    date_added = models.DateField(null=True, blank=True)
-    book_category = models.CharField(max_length=200, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True, null=True, blank=True)
+    book_category = models.ManyToManyField(Category)
 
-    class Meta:
+    class Meta: 
         ordering = ['-date_added']
 
 
     def __str__(self):
-        return self.title
+        return str(self.title)
     
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.slug)])
-
-class Author(models.Model):
-    pass
-    #author = models.ManyToManyField(Book)
-
-
-    #def __str__(self):
-    #    return self.name
-
-
-class Category(models.Model):
-    pass
-    #book_category = models.ManyToManyField(Book)
-    
-
-   # def __str__(self):
-    #    return self.book_category
-
