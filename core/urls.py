b2d302f29dcd65d32ae68c59bc7a0fa7c67f86b3
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-14 15:27:03 -0400

    going back through heroku training so need to commit push

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
